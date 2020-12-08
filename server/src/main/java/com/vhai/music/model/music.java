9ba4a281c805bca73ef67f27c34fac14735f5419
HaiVu <vhai@tma.com.vn>
2020-12-08 18:21:37 +0700

    Lombok add dependency

---
+++ server/src/main/java/com/vhai/music/model/music.java
@@ -0,0 +1,14 @@
+package com.vhai.music.model;
+import lombok.Setter;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="music")
+@Getter
+@Setter
+public class music {
+
+}
